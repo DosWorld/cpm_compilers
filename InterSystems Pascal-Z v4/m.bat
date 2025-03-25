e1484884b47e91a5bab7a87e9c1dbd2bf1b379f5
davidly <davidly_lee@hotmail.com>
2025-03-25 12:01:17 -0700

    improve comment about pasopt bug and workaround

--- InterSystems Pascal-Z v4/m.bat
+++ InterSystems Pascal-Z v4/m.bat
@@ -1,8 +1,18 @@
+@echo off
+setlocal
+
+del %1.src 2>nul
+del %1.org 2>nul
+del %1.com 2>nul
+del %1.lst 2>nul
+del %1.rel 2>nul
+
 ntvcm Pascal48 %1
 
-rem pasopt requires some uninitialized RAM to be non-zero to work. It depends on some other
-rem app having run to initialize that memory. It's a byte at BDOS - 30eh, which is 0fbeeh
-rem for NTVCM, which fills this byte for this app.
+rem pasopt requires a byte RAM to be non-zero to work. It depends on some other
+rem app having run to initialize that memory. 
+rem The byte's location is -30eH bytes from the BDOS address.
+rem NTVCM has a workaround. Comment this out if your emulator doesn't do this.
 ntvcm pasopt %1.SRC
 
 ntvcm asmbl main,%1/rel
